f42bae2dccb08d5ea294af038093657626775d46
twfc6 <72290461+twfc6@users.noreply.github.com>
2020-10-14 18:40:15 +0900

    課題提出テスト

---
+++ 2-7/index.php
@@ -0,0 +1,9 @@
+<?php
+$color = ["red" => "赤", "blue" => "青", "green"=>"緑"];
+$colors = ["red" => "赤", "blue" => "青", "green"=>"緑"];
+$colors["yellow"] = "黄色";
+
+var_dump($color);
+echo '<br>';
+var_dump($colors);
+?>
