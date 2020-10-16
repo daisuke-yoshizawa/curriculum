bccc9b57de0caa7e5dfa59ba6c3cf721e45d3847
twfc6 <72290461+twfc6@users.noreply.github.com>
2020-10-17 01:32:30 +0900

    課題提出テスト

---
+++ 2-10/index.php
@@ -0,0 +1,10 @@
+<?php 
+function getTriangleArea($base,$height,$width){
+    $area = $base * $height * $width;
+    print "縦＝5cm、横10cm、高さ8cmの直方体の体積は".$area."cm3";
+}
+
+getTriangleArea(5,10,8);
+
+
+?>
