d6891c5b9899cced11c1814fef7a93b0cf0df185
twfc6 <72290461+twfc6@users.noreply.github.com>
2020-10-17 12:37:46 +0900

    課題提出テスト

---
+++ 2-12/result.php
@@ -0,0 +1,8 @@
+<?php
+$my_name = $_POST['my_name'];
+$hope = $_POST['hope'];
+$number = $_POST['number'];
+?>
+<p>お名前<?php echo $my_name; ?></p>
+<p>ご希望商品<?php echo $hope; ?></p>
+<p>個数<?php echo $number; ?></p>
